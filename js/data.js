adcf6ae06357eec287f1c2dde71a328b431341f4
rabby6530 <you@example.com>
2023-03-06 16:00:15 +0600

    load-Spinner-complete

--- js/data.js
+++ js/data.js
@@ -1,13 +1,40 @@
-const url = `https://openapi.programming-hero.com/api/ai/tools`;
-fetch(url)
-  .then(res => res.json())
-  .then(data => displayData(data.data.tools));
+const loadData = () => {
+  ///  start loader spinner
+  toggleSpinner(true);
+
+  const url = `https://openapi.programming-hero.com/api/ai/tools`;
+  fetch(url)
+    .then(res => res.json())
+    .then(data => displayData(data.data.tools));
+
+
+}
+
+
 
 const displayData = (data) => {
   const hubContainer = document.getElementById('hub-container');
 
+  //  display only 6 data
+  if (data.length > 6) {
+    data = data.slice(0, 6);
+    const seeMore = document.getElementById('see-more');
+    seeMore.classList.remove('d-none');
+  }
+  else {
+    const seeMore = document.getElementById('see-more');
+    seeMore.classList.add('d-none');
+  }
+
+  // data limit common function
+  // const dataLimit=()=>{
+
+  // }
+
+  // display all data
+
   data.forEach(data => {
-    console.log(data);
+    // console.log(data);
     const hubDive = document.createElement('div');
     hubDive.classList.add('col');
     hubDive.innerHTML = `
@@ -27,7 +54,7 @@ const displayData = (data) => {
           <h5 class="card-title"><i class="fa-solid fa-calendar-days"></i> ${data.published_in}</h5>
           </div>
           <div>
-            <button class="bg-danger-subtle rounded-circle border border-0 " ><i class="fa-solid p-2  fa-arrow-right"></i></button>
+            <button class="bg-danger-subtle rounded-circle border border-0 " data-bs-toggle="modal" data-bs-target="#ai-details" ><i class="fa-solid p-2  fa-arrow-right"></i></button>
           </div>
         
         </div>
@@ -36,6 +63,32 @@ const displayData = (data) => {
         `
     hubContainer.appendChild(hubDive);
 
+    // stop loader
+    toggleSpinner(false);
 
   });
 }
+
+// loader operation common function
+const toggleSpinner = (isLoading) => {
+  const loaderSection = document.getElementById('loader');
+  if (isLoading) {
+
+    loaderSection.classList.remove('d-none');
+
+  }
+  else {
+
+    loaderSection.classList.add('d-none');
+  }
+
+}
+
+
+// see more button operation 
+document.getElementById('btn-see-more').addEventListener('click', function () {
+
+});
+
+loadData();
+
